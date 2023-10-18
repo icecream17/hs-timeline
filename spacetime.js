f0236bac8914b2df25e706228125c4c341471bbe
icecream17 <nguyeste008@students.garlandisd.net>
2023-10-18 13:21:37 -0500

    Create spacetime.js

---
+++ spacetime.js
@@ -0,0 +1,17 @@
+
+// The theory of relativity relates space and time together.
+// However, Homestuck breaks the laws of physics, which aren't laws so much as rules
+// that get less stable the further out you go from a universe.
+
+// For simplicity, let's have space and time can be separate properties.
+// Since time of one universe is independent from another, this is convenient.
+
+// Additionally, JavaScript only allows datetimes within 100 million seconds of 1970-1-1.
+// But since there are events that happened, say, 413 million years ago, there will need to be a workaround.
+
+// And finally, how are universes named? When does a character hear of a different universe's existence or its name?
+// Universe > Name > Fact > Timeline point > Universe
+//
+// There is a recursive structure...
+
+
